314e863b10dd1faf93f480e0fbf1a1295b20ab7f
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-05-14 09:37:18 +0700

    feat: add client get all kabkota

--- pkg/modules/tracer/kabkota/routes/get_all_kabkota.go
+++ pkg/modules/tracer/kabkota/routes/get_all_kabkota.go
@@ -2,17 +2,29 @@ package routes
 
 import (
 	"context"
+	"net/http"
 	"tracerstudy-api-gateway/pkg/pb"
+	"tracerstudy-api-gateway/pkg/utils"
 
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func GetAllKabkota(ctx *gin.Context, c pb.KabKotaServiceClient) {
-	res, err := c.GetAllKabKota(context.Background(), &emptypb.Empty{})
+	authorizationHeader := ctx.GetHeader("Authorization")
+	grpcCtx := metadata.NewOutgoingContext(context.Background(), metadata.Pairs("authorization", authorizationHeader))
+
+	res, err := c.GetAllKabKota(grpcCtx, &emptypb.Empty{})
 
 	if err != nil {
-		ctx.AbortWithError(http.StatusInternalServerError, err)
+		errResp := utils.GetGrpcError(err)
+		ctx.AbortWithStatusJSON(
+			errResp.Code,
+			errResp,
+		)
 		return
 	}
+
+	ctx.JSON(http.StatusOK, &res)
 }
