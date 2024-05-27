f866125a122ba905ee1469dd1a251bdbe65da41d
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-05-27 13:13:29 +0700

    feat: add client responden update from siak api & create new

---
+++ pkg/modules/tracer/responden/routes/create_responden.go
@@ -0,0 +1,46 @@
+package routes
+
+import (
+	"context"
+	"net/http"
+	"tracerstudy-api-gateway/pkg/pb"
+	"tracerstudy-api-gateway/pkg/utils"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc/metadata"
+)
+
+type CreateRespondenRequestBody struct {
+	Nim string `json:"nim"`
+}
+
+func CreateResponden(ctx *gin.Context, c pb.RespondenServiceClient) {
+	authorizationHeader := ctx.GetHeader("Authorization")
+	grpcCtx := metadata.NewOutgoingContext(context.Background(), metadata.Pairs("authorization", authorizationHeader))
+
+	b := CreateRespondenRequestBody{}
+
+	if err := ctx.BindJSON(&b); err != nil {
+		errResp := utils.NewErrorResponse(http.StatusBadRequest, "Bad Request", "Invalid request body")
+		ctx.AbortWithStatusJSON(
+			http.StatusBadRequest,
+			errResp,
+		)
+		return
+	}
+
+	res, err := c.CreateResponden(grpcCtx, &pb.CreateRespondenRequest{
+		Nim: b.Nim,
+	})
+
+	if err != nil {
+		errResp := utils.GetGrpcError(err)
+		ctx.AbortWithStatusJSON(
+			errResp.Code,
+			errResp,
+		)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, &res)
+}
---
+++ pkg/modules/tracer/responden/routes/update_responden_from_siak.go
@@ -0,0 +1,33 @@
+package routes
+
+import (
+	"context"
+	"net/http"
+	"tracerstudy-api-gateway/pkg/pb"
+	"tracerstudy-api-gateway/pkg/utils"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc/metadata"
+)
+
+func UpdateRespondenFromSiak(ctx *gin.Context, c pb.RespondenServiceClient) {
+	nim := ctx.Param("nim")
+
+	authorizationHeader := ctx.GetHeader("Authorization")
+	grpcCtx := metadata.NewOutgoingContext(context.Background(), metadata.Pairs("authorization", authorizationHeader))
+
+	res, err := c.UpdateRespondenFromSiak(grpcCtx, &pb.UpdateRespondenFromSiakRequest{
+		Nim: nim,
+	})
+
+	if err != nil {
+		errResp := utils.GetGrpcError(err)
+		ctx.AbortWithStatusJSON(
+			errResp.Code,
+			errResp,
+		)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, &res)
+}
