fd1a7d4feb165e578245ba3ac15ec840d9ba4dcf
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-05-27 15:12:30 +0700

    feat: add client userstudy get userstudy rekap by prodi

---
+++ pkg/modules/tracer/userstudy/routes/get_userstudy_rekap_by_prodi.go
@@ -0,0 +1,33 @@
+package routes
+
+import (
+	"context"
+	"net/http"
+	"tracerstudy-api-gateway/pkg/pb"
+	"tracerstudy-api-gateway/pkg/utils"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc/metadata"
+)
+
+func GetUserStudyRekapByProdi(ctx *gin.Context, c pb.UserStudyServiceClient) {
+	authorizationHeader := ctx.GetHeader("Authorization")
+	grpcCtx := metadata.NewOutgoingContext(context.Background(), metadata.Pairs("authorization", authorizationHeader))
+
+	kode := ctx.Param("kode")
+
+	res, err := c.GetUserStudyRekapByProdi(grpcCtx, &pb.GetUserStudyRekapByProdiRequest{
+		KodeProdi: kode,
+	})
+
+	if err != nil {
+		errResp := utils.GetGrpcError(err)
+		ctx.AbortWithStatusJSON(
+			errResp.Code,
+			errResp,
+		)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, &res)
+}
