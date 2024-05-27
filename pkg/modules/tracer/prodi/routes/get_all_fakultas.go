fc26d30fb90710c25cdffdd449db997282ca7128
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-05-27 11:31:42 +0700

    feat: add client get all fakultas

---
+++ pkg/modules/tracer/prodi/routes/get_all_fakultas.go
@@ -0,0 +1,30 @@
+package routes
+
+import (
+	"context"
+	"net/http"
+	"tracerstudy-api-gateway/pkg/pb"
+	"tracerstudy-api-gateway/pkg/utils"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func GetAllFakultas(ctx *gin.Context, c pb.ProdiServiceClient) {
+	authorizationHeader := ctx.GetHeader("Authorization")
+	grpcCtx := metadata.NewOutgoingContext(context.Background(), metadata.Pairs("authorization", authorizationHeader))
+
+	res, err := c.GetAllFakultas(grpcCtx, &emptypb.Empty{})
+
+	if err != nil {
+		errResp := utils.GetGrpcError(err)
+		ctx.AbortWithStatusJSON(
+			errResp.Code,
+			errResp,
+		)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, &res)
+}
