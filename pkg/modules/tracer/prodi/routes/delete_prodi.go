98b93103749a252664bcc0bf76ae48eeb5717519
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-05-27 11:40:07 +0700

    feat: add client delete prodi

---
+++ pkg/modules/tracer/prodi/routes/delete_prodi.go
@@ -0,0 +1,33 @@
+package routes
+
+import (
+	"context"
+	"net/http"
+	"tracerstudy-api-gateway/pkg/pb"
+	"tracerstudy-api-gateway/pkg/utils"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc/metadata"
+)
+
+func DeleteProdi(ctx *gin.Context, c pb.ProdiServiceClient) {
+	authorizationHeader := ctx.GetHeader("Authorization")
+	grpcCtx := metadata.NewOutgoingContext(context.Background(), metadata.Pairs("authorization", authorizationHeader))
+
+	kode := ctx.Param("kode")
+
+	res, err := c.DeleteProdi(grpcCtx, &pb.GetProdiByKodeRequest{
+		Kode: kode,
+	})
+
+	if err != nil {
+		errResp := utils.GetGrpcError(err)
+		ctx.AbortWithStatusJSON(
+			errResp.Code,
+			errResp,
+		)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, &res)
+}
