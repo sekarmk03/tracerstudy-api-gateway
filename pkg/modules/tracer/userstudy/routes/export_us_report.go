c2bb9f5ad675bd113cde31e9b348f0615db201c0
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-05-27 14:27:11 +0700

    feat: add client userstudy export report excel

---
+++ pkg/modules/tracer/userstudy/routes/export_us_report.go
@@ -0,0 +1,30 @@
+package routes
+
+import (
+	"context"
+	"net/http"
+	"tracerstudy-api-gateway/pkg/pb"
+	"tracerstudy-api-gateway/pkg/utils"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func ExportUSReport(ctx *gin.Context, c pb.UserStudyServiceClient) {
+	authorizationHeader := ctx.GetHeader("Authorization")
+	grpcCtx := metadata.NewOutgoingContext(context.Background(), metadata.Pairs("authorization", authorizationHeader))
+
+	res, err := c.ExportUSReport(grpcCtx, &emptypb.Empty{})
+
+	if err != nil {
+		errResp := utils.GetGrpcError(err)
+		ctx.AbortWithStatusJSON(
+			errResp.Code,
+			errResp,
+		)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, &res)
+}
