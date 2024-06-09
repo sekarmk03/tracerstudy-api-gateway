301d29ef5a3c0c9a014044719e711b05b9f383f9
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-06-09 17:51:10 +0700

    feat: remove chaining get responden

--- pkg/modules/tracer/responden/routes/get_responden_by_nim.go
+++ pkg/modules/tracer/responden/routes/get_responden_by_nim.go
@@ -10,71 +10,23 @@ import (
 	"google.golang.org/grpc/metadata"
 )
 
-func GetRespondenByNim(ctx *gin.Context, cr pb.RespondenServiceClient) {
+func GetRespondenByNim(ctx *gin.Context, c pb.RespondenServiceClient) {
 	authorizationHeader := ctx.GetHeader("Authorization")
 	grpcCtx := metadata.NewOutgoingContext(context.Background(), metadata.Pairs("authorization", authorizationHeader))
 	
 	nim := ctx.Param("nim")
 
-	res, err := cr.GetRespondenByNim(grpcCtx, &pb.GetRespondenByNimRequest{
+	res, err := c.GetRespondenByNim(grpcCtx, &pb.GetRespondenByNimRequest{
 		Nim: nim,
 	})
 
 	if err != nil {
 		errResp := utils.GetGrpcError(err)
-		if errResp.Code == http.StatusNotFound {
-			// create responden
-			_, err := cr.CreateResponden(grpcCtx, &pb.CreateRespondenRequest{
-				Nim: nim,
-			})
-
-			if err != nil {
-				errResp := utils.GetGrpcError(err)
-				ctx.AbortWithStatusJSON(
-					errResp.Code,
-					errResp,
-				)
-				return
-			}
-
-			resUpdate, err := cr.UpdateRespondenFromSiak(grpcCtx, &pb.UpdateRespondenFromSiakRequest{
-				Nim: nim,
-			})
-
-			if err != nil {
-				errResp := utils.GetGrpcError(err)
-				ctx.AbortWithStatusJSON(
-					errResp.Code,
-					errResp,
-				)
-				return
-			}
-
-			ctx.JSON(http.StatusOK, &resUpdate)
-		} else {
-			ctx.AbortWithStatusJSON(
-				errResp.Code,
-				errResp,
-			)
-			return
-		}
-	}
-
-	if res.Data.StatusUpdate == 0 {
-		resUpdate, err := cr.UpdateRespondenFromSiak(grpcCtx, &pb.UpdateRespondenFromSiakRequest{
-			Nim: nim,
-		})
-
-		if err != nil {
-			errResp := utils.GetGrpcError(err)
-			ctx.AbortWithStatusJSON(
-				errResp.Code,
-				errResp,
-			)
-			return
-		}
-
-		ctx.JSON(http.StatusOK, &resUpdate)
+		ctx.AbortWithStatusJSON(
+			errResp.Code,
+			errResp,
+		)
+		return
 	}
 
 	ctx.JSON(http.StatusOK, &res)
