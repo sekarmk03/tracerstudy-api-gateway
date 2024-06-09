73de91f4c59693a65f2a9da979e91f20679dec72
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-06-09 22:39:49 +0700

    feat: add route delete post

---
+++ pkg/modules/post/post/routes/delete_post.go
@@ -0,0 +1,43 @@
+package routes
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+	"tracerstudy-api-gateway/pkg/pb"
+	"tracerstudy-api-gateway/pkg/utils"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc/metadata"
+)
+
+func DeletePost(ctx *gin.Context, c pb.PostServiceClient) {
+	id, err := strconv.ParseUint(ctx.Param("id"), 10, 64)
+
+	if err != nil {
+		errResp := utils.NewErrorResponse(http.StatusBadRequest, "Bad Request", "Failed to convert id to int")
+		ctx.AbortWithStatusJSON(
+			http.StatusBadRequest,
+			errResp,
+		)
+		return
+	}
+
+	authorizationHeader := ctx.GetHeader("Authorization")
+	grpcCtx := metadata.NewOutgoingContext(context.Background(), metadata.Pairs("authorization", authorizationHeader))
+
+	res, err := c.DeletePost(grpcCtx, &pb.GetPostByIdRequest{
+		Id: id,
+	})
+
+	if err != nil {
+		errResp := utils.GetGrpcError(err)
+		ctx.AbortWithStatusJSON(
+			errResp.Code,
+			errResp,
+		)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, &res)
+}
