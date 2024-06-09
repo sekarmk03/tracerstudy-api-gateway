bbc759a710dcc103059688253833af0cc4421805
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-06-09 22:55:39 +0700

    feat: add route get comment by id

---
+++ pkg/modules/post/comment/routes/get_comment_by_id.go
@@ -0,0 +1,41 @@
+package routes
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+	"tracerstudy-api-gateway/pkg/pb"
+	"tracerstudy-api-gateway/pkg/utils"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc/metadata"
+)
+
+func GetCommentById(ctx *gin.Context, c pb.CommentServiceClient) {
+	authorizationHeader := ctx.GetHeader("Authorization")
+	grpcCtx := metadata.NewOutgoingContext(context.Background(), metadata.Pairs("authorization", authorizationHeader))
+
+	id, err := strconv.ParseUint(ctx.Param("id"), 10, 64)
+
+	if err != nil {
+		errResp := utils.NewErrorResponse(http.StatusBadRequest, "Bad Request", "Failed to convert id to int")
+		ctx.AbortWithStatusJSON(
+			http.StatusBadRequest,
+			errResp,
+		)
+		return
+	}
+
+	res, err := c.GetCommentById(grpcCtx, &pb.GetCommentByIdRequest{Id: id})
+
+	if err != nil {
+		errResp := utils.GetGrpcError(err)
+		ctx.AbortWithStatusJSON(
+			errResp.Code,
+			errResp,
+		)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, &res)
+}
