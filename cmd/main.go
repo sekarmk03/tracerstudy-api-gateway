479b96fb95269be681d29fd9f9df81d86db80a93
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-06-10 17:16:45 +0700

    feat: add authorization allow header

--- cmd/main.go
+++ cmd/main.go
@@ -31,7 +31,7 @@ func main() {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
-		AllowHeaders:     []string{"Origin", "Content-Type"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
